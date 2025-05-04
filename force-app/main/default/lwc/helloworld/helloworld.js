b0682216c24a32430d358fc296b4fa11423e8280
Nithya Narayanan <NithyaNarayananCVR@gmail.com>
2025-05-04 07:28:45 +0530

    Working with LWC

--- force-app/main/default/lwc/helloworld/helloworld.js
+++ force-app/main/default/lwc/helloworld/helloworld.js
@@ -1,6 +1,8 @@
 import { LightningElement } from 'lwc';
+import myImage from '@salesforce/resourceUrl/NNImage';
 export default class HelloWorld extends LightningElement {
         greeting = 'World';
+        imageUrl = myImage;
         changeHandler(event) {
         this.greeting = event.target.value;
         }
